ab52aef2e649ee4bdcc57262ed6b52309b5b4b63
dwbarnard <35636703+dwbarnard@users.noreply.github.com>
2018-02-04 18:24:05 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Assignment #1  Person");
+
+			Person myPerson1 = new Person();
+
+			Console.WriteLine("Enter The Person's First Name:");
+			myPerson1.FirstName = Console.ReadLine();
+			Console.WriteLine("Enter The Person's Last Name:");
+			myPerson1.LastName = Console.ReadLine();
+			Console.WriteLine("Enter The Person's Age:");
+			try
+			{
+				myPerson1.Age = Convert.ToInt32(Console.ReadLine());
+			}
+			catch
+			{
+				Console.WriteLine("Age should be an Integer");
+			}
+			Console.WriteLine("Does person have a spouse? Enter y or n");
+			string hasSpouse = Console.ReadLine();
+			if (hasSpouse == "Y" || hasSpouse == "y")
+			{
+				Person mySpouse = new Person();
+				mySpouse.LastName = myPerson1.LastName;
+				mySpouse.Spouse = myPerson1;
+				myPerson1.Spouse = mySpouse;
+				Console.WriteLine("Enter Spouse'e First Name:");
+				mySpouse.FirstName = Console.ReadLine();
+				Console.WriteLine("Enter Spouse'e Age:");
+				try
+				{
+					mySpouse.Age = Convert.ToInt32(Console.ReadLine());
+				}
+				catch
+				{
+					Console.WriteLine("Age should be an Integer");
+				}
+
+			}
+
+			Console.WriteLine();
+
+
+			myPerson1.DisplayPersonInfo(1);
+
+			Console.WriteLine("\n\n\nPress Enter key to end program");
+			Console.ReadLine();
+		}
+
+
+	}
+}
---
+++ person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+	public class Person
+	{
+
+		// member properties
+		public int Age = 0;
+		public string FirstName = "";
+		public string LastName = "";
+		public Person Spouse;
+
+		// class static properties
+		static double AverageAge = 0.0;
+		static Person YoungestSingle;
+		static Person YoungestMarried;
+		static Person OldestSingle;
+		static Person OlsestMarried;
+
+
+		//public Person(Person Spouse)
+		//{
+		//	this.Spouse = Spouse;
+		//}
+
+
+		public void DisplayPersonInfo(int personId)
+		{
+			Console.WriteLine("Person #" + personId + " First Name \t\t:  " + this.FirstName);
+			Console.WriteLine("Person #" + personId + " Last Name \t\t:  " + this.LastName);
+			Console.WriteLine("Person #" + personId + " Age \t\t\t:  " + this.Age);
+			if (this.Spouse != null)
+			{
+				Console.WriteLine("Person #" + personId + " Married?   \t\t:  yes");
+				Console.WriteLine("Person #" + personId + " Spouse First Name   \t:  " + this.Spouse.FirstName);
+				Console.WriteLine("Person #" + personId + " Spouse Age   \t\t:  " + this.Spouse.Age);
+			}
+			else
+			{
+				Console.WriteLine("Person #" + personId + "Married?   \t\t:  no");
+			}
+
+		}
+
+		public void GetPersionInfo()
+		{
+			Console.WriteLine("Enter The Person's First Name:");
+			this.FirstName = Console.ReadLine();
+			Console.WriteLine("Enter The Person's Last Name:");
+			this.LastName = Console.ReadLine();
+			Console.WriteLine("Enter The Person's Age:");
+			try
+			{
+				this.Age = Convert.ToInt32(Console.ReadLine());
+			}
+			catch
+			{
+				Console.WriteLine("Age should be an Integer");
+			}
+			Console.WriteLine();
+		}
+	}
+}
